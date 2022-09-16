5fcb9dd768791ce477343f98df86b88432b43e7f
Julia <jskredova@yandex.ru>
2022-09-17 00:27:13 +0300

    add Task String Length

---
+++ Task_Final/Task_String_Length/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillArray1(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine()!;
+    }
+}
+
+int FindSize2(string[] arr1)
+{
+    int j = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            j++;
+        }
+    }
+    return j;
+}
+
+void FillArray2(string[] arr1, string[] arr2)
+{
+    int j = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[j] = arr1[i];            
+            j++;
+        }
+    }
+}
+
+Console.Write("Укажите количество элементов: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array1 = new string[size];
+Console.Write("Заполните массив: ");
+FillArray1(array1);
+
+string[] array2 = new string[(FindSize2(array1))];
+
+Console.WriteLine();
+FillArray2(array1, array2);
+Console.Write(string.Join(" ", array2));
